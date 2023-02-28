37de12d069d32523b8644df2660f2dea893c0c3c
Tuvelout <tuvelout@gmail.com>
2023-02-28 18:53:18 +0000

    Update index.js

--- Ficha_1/index.js
+++ Ficha_1/index.js
@@ -1,1 +1,34 @@
-consolo.log('hello world')
+console.log("hello world")
+
+function minha_funcao(nota_pratica, nota_teorica)
+{
+    var nota = nota_pratica * 0.6 + nota_teorica * 0.4;
+    if (nota >= 9.5)
+        console.log("Aprovado");
+    else
+        console.log("reprovado");
+}
+
+
+minha_funcao(11,5)
+
+// function minha_funcao(nota_pratica, nota_teorica)
+// {
+//     var nota = nota_pratica * 0.6 + nota_teorica * 0.4;
+//     if (nota >= 9.5)
+//         return true;
+//     else
+//         return false;
+// }
+
+// var approval =
+
+function nova_funcao(nome_mes){
+    switch (nova_funcao) {
+        case 1:
+            console.log("Janeiro");
+            
+    }
+}
+
+nova_funcao(3)
