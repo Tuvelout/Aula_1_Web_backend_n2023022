afdb3f176e7be7a44b79a113fe3de236450048cf
Tuvelout <tuvelout@gmail.com>
2023-03-07 18:00:54 +0000

    Update index.js

--- Ficha_1/index.js
+++ Ficha_1/index.js
@@ -36,13 +36,15 @@ function nova_funcao(nome_mes){
 
 nova_funcao(1)
 
-function operacoes(num1,num2,operador){
-    var resultado = {}
-    resultado = num1 operador num2;
-    console.log(resultado)
-}
+// function operacoes(num1,num2,operador){
+//     var resultado = {}
+//     resultado = num1 operador num2;
+//     console.log(resultado)
+// }
 
-//operacoes(1,2,'*')
+// operacoes(1,2,'*')
+
+//ex7
 
 function operacoes(num1,num2,operador){
     var resultado = {}
@@ -65,4 +67,89 @@ function operacoes(num1,num2,operador){
     return resultado;
 }
 
-operacoes(1,2,'*')
+//console.log(operacoes(1,2,'*'))
+
+
+function multiplos_de_cinco(){
+    //var i = 0
+    let res = 5
+    for (let i = 1; res <= 20; res = i * 5){
+        console.log(res) // executa est vez uma vez primeiro
+        i += 1
+    }
+}
+
+multiplos_de_cinco()
+// ex9
+
+function soma_100_primeiros_inteiros(cem){
+    var sum = 0;
+    for (let i = 0; i < cem; i++) {
+        sum += i;
+    }
+    return sum
+}
+
+console.log(soma_100_primeiros_inteiros(100))
+
+//ex10
+
+function fatorial(num) {
+    var fact = 1;
+    for (let i = 2; i < num; i++) {
+        fact*=i;
+    }
+    return fact;
+}
+
+console.log(fatorial(2))
+
+// ex11.minimo
+
+function minimo(array){
+    var min = array[0];
+    var l = array.length;
+    for (var i = 1; i < l ; i++){
+        if (i < min){
+            min = array[i]
+        }
+    }
+    return min
+}
+
+var lista = [2,3,7,0,1]
+var min = minimo(lista)
+console.log(min) //mostar o minimo
+
+//ex11.máximo
+
+function maximo(array){
+    var max = array[0];
+    var l = array.length;
+    for (var i = 1; i < l ; i++){
+        if (i > max){
+            max = array[i]
+        }
+    }
+    return max
+}
+
+var lista = [2,3,7,0,1]
+var max = maximo(lista)
+console.log(max) //mostar o máximo
+
+//ex11.média procura como o i funciona para obter o valor da lista
+
+function media(array){
+    var l = array.length;
+    var soma = 0
+    for (var i = 1; i < l ; i++){
+        if (i > max){
+            max = array[i]
+        }
+    }
+    return max
+}
+
+var lista = [2,3,7,0,1]
+console.log(media) //mostar o máximo
